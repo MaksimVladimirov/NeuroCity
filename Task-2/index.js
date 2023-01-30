4c261d6d7607cff04861082c1cb7031f1fdff3b6
Maksim Vladimirov <vl4dimirov@gmail.com>
2023-01-30 20:06:29 +0300

    feat: Task2

---
+++ Task-2/index.js
@@ -0,0 +1,15 @@
+circle.onclick = function(){
+  this.classList.add('circle-animated')
+}
+
+rectangle.onclick = function(){
+  this.classList.add("rectangle-animated");
+}
+
+square.onclick = function(){
+  this.classList.add("square__animated");
+}
+
+triangle.onclick = function(){
+  this.classList.add("triangle-animated");
+}
